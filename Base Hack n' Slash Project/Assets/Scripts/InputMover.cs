562baccdaefcf02e0e9167c22b707b71ebb53d9f
Jared <ebelsj2@ferris.edu>
2020-04-07 19:37:45 -0400

    Enemy AI - Splitting
    The enemies now split and lower a level on a trigger

---
+++ Base Hack n' Slash Project/Assets/Scripts/InputMover.cs
@@ -0,0 +1,49 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMover : MonoBehaviour {
+
+    //LineRenderer line;
+    InputManager.InputConfig playerController;
+
+    public Rigidbody rb;
+    private float forwardAccel = 6f;
+    private float speed = 100f;
+
+
+	void Start () {
+        //line = GetComponent<LineRenderer>();
+
+        playerController = InputManager.player1;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetComponent<MeshRenderer>().material.color = playerController.OnJump(true) ? Color.yellow : Color.white;
+
+        Vector3 input = playerController.GetAxis1();
+        transform.position = input;
+        //line.SetPosition(1, input);
+
+        if (input.sqrMagnitude > 1) input.Normalize();
+
+// HEAD
+       
+
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(playerController.GetAxis1x(false) * speed * Time.deltaTime, 0f, forwardAccel);
+        print(rb.velocity);
+        if (rb.velocity.z >= 35) rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 35f);
+        else forwardAccel = 6f;
+        //print("Player: " + transform.position);
+        
+    }
+   
+
+    
+}
---
+++ Base Hack n' Slash Project/Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    public Rigidbody rb;
+    private float coolspeed = 750f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent < Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(Input.GetAxis("Horizontal") * coolspeed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * coolspeed * Time.deltaTime);
+        //print("velocity = " + rb.velocity);
+        if (rb.velocity.z >= 100)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 100f);
+        }
+        
+    }
+}
